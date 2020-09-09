4d37422a043204cbee01602bc886d24f35fe5e40
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2020-09-09 18:53:39 -0300

    CLI 'Séria'

---
+++ src/br/cefetmg/address/CLI/Main.java
@@ -0,0 +1,9 @@
+package br.cefetmg.address.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        // CLI: Command Line Interface
+
+        System.out.println("Bom dia 😀");
+    }
+}
