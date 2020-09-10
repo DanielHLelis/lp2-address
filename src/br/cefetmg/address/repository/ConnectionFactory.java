133329d6b679f4ab98d33e69824145b0b8ea6cd7
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2020-09-10 11:44:11 -0300

    updated: Persist -> Repository; added env variables to ConnectionFactory

---
+++ src/br/cefetmg/address/repository/ConnectionFactory.java
@@ -0,0 +1,78 @@
+package br.cefetmg.address.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+TO-DO
+
+- Adicionar arquivo .conf e/ou variável
+*/
+
+public class ConnectionFactory {
+
+    private final static String dbDriver = "org.postgresql.Driver";
+    private final static String protocol = "jdbc:postgresql";
+
+    private final static String DEFAULT_HOST = "localhost";
+    private final static String DEFAULT_PORT = "5432";
+    private final static String DEFAULT_DB = "address";
+    private final static String DEFAULT_USER = "postgres";
+    private final static String DEFAULT_PASS = "postgres";
+
+    private final static Map<String, String> PARAMS = getParams();
+
+    private static String getUrl() {
+        return String.format("%s://%s:%s/%s?useUnicode=true&amp;characterEncoding=utf8", protocol, PARAMS.get("host"), PARAMS.get("port"), PARAMS.get("db"));
+    }
+
+    private static Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<>();
+
+        // DEFAULT
+
+        params.put("host", DEFAULT_HOST);
+        params.put("port", DEFAULT_PORT);
+        params.put("db", DEFAULT_DB);
+        params.put("user", DEFAULT_USER);
+        params.put("pass", DEFAULT_PASS);
+
+        // ENV
+
+        String envHost = System.getenv("DB_HOST");
+        String envPort = System.getenv("DB_PORT");
+        String envDb = System.getenv("DB_DATABASE");
+        String envUser = System.getenv("DB_USER");
+        String envPass = System.getenv("DB_PASS");
+
+        if (envHost != null && !envHost.equals("")){
+            params.put("host", envHost);
+        }
+
+        if (envPort != null && !envPort.equals("")){
+            params.put("port", envPort);
+        }
+
+        if (envDb != null && !envDb.equals("")){
+            params.put("db", envDb);
+        }
+
+        if (envUser != null && !envUser.equals("")){
+            params.put("user", envUser);
+        }
+
+        if (envPass != null && !envPass.equals("")){
+            params.put("pass", envPass);
+        }
+
+        return params;
+    }
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(dbDriver);
+        return DriverManager.getConnection(getUrl(), PARAMS.get("user"), PARAMS.get("pass"));
+    }
+}
---
+++ src/br/cefetmg/address/repository/Repository.java
@@ -0,0 +1,18 @@
+package br.cefetmg.address.repository;
+
+import br.cefetmg.address.models.Model;
+
+import java.util.List;
+
+public interface Repository<M extends Model> {
+    void insert(M model) throws RepositoryException;
+
+    void update(M model) throws RepositoryException;
+
+    M delete(Long id) throws RepositoryException;
+
+    M getById(Long id) throws RepositoryException;
+
+    List<M> listAll() throws RepositoryException;
+
+}
